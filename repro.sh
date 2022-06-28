523d857d369a0be14d982ad183163bed5a2032c7
Luke Bakken <luke@bakken.io>
2022-06-28 06:15:18 -0700

    Be more explicit in setting Erlang version

--- repro.sh
+++ repro.sh
@@ -4,29 +4,50 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# shellcheck disable=SC2155
 readonly dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 git submodule update --init
 
-make -C "$dir/rabbitmq-perf-test"
+function set_erlang_version
+{
+    asdf local erlang 24.3.4 && asdf local elixir 1.12.3-otp-24
+    asdf current
+    echo 'ERLANG VERSION:'
+    erl -noinput -eval 'F=filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"]),io:format("~p~n",[
+file:read_file(F)]),halt().'
+}
 
 {
-    cd "$dir/rabbitmq-server" && asdf local erlang 24.3.4 && asdf local elixir 1.12.3-otp-24
+    cd "$dir/rabbitmq-server"
+    set_erlang_version
     make FULL=1
 } &
 
-make -C "$dir/rabbitmq-perf-test" compile &
+{
+    cd "$dir/rabbitmq-perf-test"
+    make compile
+} &
 
 wait
 
-make -C "$dir/rabbitmq-server" RABBITMQ_CONFIG_FILE="$dir/rabbitmq.conf" PLUGINS='rabbitmq_management rabbitmq_top' NODES=3 start-cluster
+{
+    cd "$dir/rabbitmq-server"
 
-(cd "$dir/rabbitmq-server" && ./sbin/rabbitmqctl --node rabbit-1 set_policy --apply-to queues \
-    --priority 0 policy-0 ".*" '{"ha-mode":"all", "ha-sync-mode": "automatic", "queue-mode": "lazy"}')
+    set_erlang_version
 
-(cd "$dir/rabbitmq-server" && ./sbin/rabbitmqctl --node rabbit-1 set_policy --apply-to queues \
-    --priority 1 policy-1 ".*" '{"ha-mode":"all", "ha-sync-mode": "automatic"}')
+    make RABBITMQ_CONFIG_FILE="$dir/rabbitmq.conf" PLUGINS='rabbitmq_management rabbitmq_top' NODES=3 start-cluster
 
-make -C "$dir/rabbitmq-perf-test" ARGS='--consumers 0 --producers 1 --predeclared --queue gh-5086 --pmessages 1500000 --size 1024' run
+    ./sbin/rabbitmqctl --node rabbit-1 set_policy --apply-to queues \
+        --priority 0 policy-0 ".*" '{"ha-mode":"all", "ha-sync-mode": "automatic", "queue-mode": "lazy"}'
 
-(cd "$dir/rabbitmq-server" && ./sbin/rabbitmqctl --node rabbit-1 clear_policy policy-1)
+    ./sbin/rabbitmqctl --node rabbit-1 set_policy --apply-to queues \
+        --priority 1 policy-1 ".*" '{"ha-mode":"all", "ha-sync-mode": "automatic"}'
+}
+
+make -C "$dir/rabbitmq-perf-test" ARGS='--consumers 0 --producers 1 --predeclared --queue gh-5086 --pmessages 2000000 --size 1024' run
+
+{
+    cd "$dir/rabbitmq-server"
+    ./sbin/rabbitmqctl --node rabbit-1 clear_policy policy-1
+}
